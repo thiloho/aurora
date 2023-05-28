7988f2c1a17c05153b6d8cf69a7c0b18626215ba
thiloho <123883702+thiloho@users.noreply.github.com>
2023-05-28 16:39:40 +0200

    Change site url for og:image

--- astro.config.ts
+++ astro.config.ts
@@ -5,6 +5,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://aurora.thilohohlt.com",
   compressHTML: true,
   experimental: {
     assets: true,
